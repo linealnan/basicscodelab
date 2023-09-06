8d78baaa4c29f2f2f5476ecd07e1a8d83175ff26
Dmitriy Shkatov <DmShkatov@korusconsulting.ru>
2023-09-06 16:02:52 +0300

    [NF] Jetpack Compose basics 11. Animating your list

--- app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.basicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -21,11 +24,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.basicscodelab.ui.theme.BasicsCodelabTheme
 import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.saveable.rememberSaveable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,7 +50,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyApp(modifier: Modifier = Modifier) {
 
-    var shouldShowOnbording by remember { mutableStateOf(true) }
+    var shouldShowOnbording by rememberSaveable { mutableStateOf(true) }
+
     Surface(modifier) {
         if (shouldShowOnbording) {
             OnbordingScreen(onContinueCliked = { shouldShowOnbording = false })
@@ -93,8 +97,14 @@ fun OnbordingScreen(
 fun Greeting(name: String, modifier: Modifier = Modifier) {
 
     val expanded = remember { mutableStateOf(false) }
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    val extraPadding by animateDpAsState(
+        if (expanded.value) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
 
+    )
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier= Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
